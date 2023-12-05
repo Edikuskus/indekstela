248635dbe5f4be3ab5343d94ade3065e2c8cc072
Эдик <@DESKTOP-J1K4QFI>
2023-12-05 17:28:50 +0200

    Add project files.

---
+++ indekstela/indekstela.py
@@ -0,0 +1,39 @@
+﻿
+try:
+    print("Tere! Olen sinu uus sõber - Python!")
+
+    nimi = input("Sisesta oma nimi: ")
+    print(nimi+", oi kui ilus nimi!")
+
+    vastus = int(input(nimi + "! Kas leian Sinu keha indeksi? 0-ei, 1-jah => "))
+
+    if vastus == 1:
+        pikkus = int(input("Sisesta oma pikkus (cm): "))
+        mass = float(input("Sisesta oma mass (kg): "))
+
+        indeks = mass / (0.01 * pikkus)**2
+
+        print(nimi + "! Sinu keha indeks on: {:.1f}".format(indeks))
+        if indeks < 16:
+            print("Tervisele ohtlik alakaal")
+        elif 16 <= indeks < 19:
+            print("Alakaal")
+        elif 19 <= indeks < 25:
+            print("Normaalkaal")
+        elif 25 <= indeks < 30:
+            print("?lekaal")
+        elif 30 <= indeks < 35:
+            print("Rasvumine")
+        elif 35 <= indeks < 40:
+            print("Tugev rasvumine")
+        else:
+            print("Tervisele ohtlik rasvumine")
+
+    else:
+        print("Kahju! See on viga kasulik info!")
+
+except ValueError:
+    print("Viga! Sisesta palun arvulised v??rtused.")
+
+finally:
+    print("Kohtumiseni, " + nimi + "! Igavesti Sinu, Python!")
